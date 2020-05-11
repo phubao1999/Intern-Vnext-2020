2d89428468c26e6d4e32174331cec4fc8423feff
Bao PT <phutuongbao1999@gmail.com>
2020-05-11 10:21:46 +0700

    Add login by gg

--- fe-basic/index.js
+++ fe-basic/index.js
@@ -78,6 +78,16 @@ const initFunc = async () => {
     await checkLogin()
 }
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
+
 initFunc();
 
 const loginButton = document.getElementById('btn-login');
